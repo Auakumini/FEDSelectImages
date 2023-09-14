f01737191dc0cc546efc46d018b546e482ea3179
Alexander Kristian Melberg Kumini <202110452@post.au.dk>
2023-09-14 18:01:11 +0200

    Add project files.

---
+++ SelectImages/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.ObjectModel;
+using SelectImages.Models;
+
+namespace SelectImages
+{
+    public partial class MainPage : ContentPage
+    {
+
+        private string _imagePath = "";
+        public ObservableCollection<ImageInfo> Images { get; set; } = new();
+        public MainPage()
+        {
+            InitializeComponent();
+    }
+
+        private async void selectImageBtn_Clicked(object sender, EventArgs e)
+        {
+            var image = await FilePicker.Default.PickAsync(new PickOptions
+            {
+                PickerTitle = "Pick Image",
+                FileTypes = FilePickerFileType.Images
+            });
+            if (image != null)
+            {
+                _imagePath = image.FullPath.ToString();
+                SelectedImage.Source = _imagePath;
+            }
+        }
+
+        private async void addImageBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SelectImages/Models/ImageInfo.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SelectImages.Data;
+
+namespace SelectImages.Models
+{
+    public class ImageInfo
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Path { get; set; }
+        public string Description { get; set; }
+    }
+}
